c1118fa6a6da0e28153b0c8d44d96227ef9ab7f5
Pavanbn03 <pavanbn03@gmail.com>
2020-01-27 16:25:22 +0530

    paypal key added

--- src/components/Cart/PayPalButton.jsx
+++ src/components/Cart/PayPalButton.jsx
@@ -31,7 +31,7 @@ export default class MyApp extends React.Component {
         // Document on Paypal's currency code: https://developer.paypal.com/docs/classic/api/currency_codes/
         // let config=require('../../../env.json')
         const client = {
-            sandbox:"Aa_XMQr9mQrfAgzmf1NvTLcETQ268e-l3YEAvgUV-9hGgwlDSqHoVi4pmgDuhGn8D3jcIjExD_NPxsCH",
+            sandbox:process.env.REACT_APP_APP_ID,
             production: 'YOUR-PRODUCTION-APP-ID',
         }
         // In order to get production's app-ID, you will have to send your app to Paypal for approval first
